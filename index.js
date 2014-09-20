213250435a55eb3317e95da69f24e04fb7616f50
Kevin.Xu <cmxu@kabam.com>
2014-09-20 17:57:48 +0800

    [ADDED] redis wrapper added

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ var Server  = require('./lib/server');
 package = require("./package");
 
 //program
-  //.version('0.0.1')
-  //.option('-p, --peppers', 'Add peppers')
-  //.option('-P, --pineapple', 'Add pineapple')
-  //.option('-b, --bbq', 'Add bbq sauce')
-  //.option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
-  //.parse(process.argv);
+//.version('0.0.1')
+//.option('-p, --peppers', 'Add peppers')
+//.option('-P, --pineapple', 'Add pineapple')
+//.option('-b, --bbq', 'Add bbq sauce')
+//.option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+//.parse(process.argv);
 
 //console.log('you ordered a pizza with:');
 //if (program.peppers) console.log('  - peppers');
@@ -27,3 +27,7 @@ package = require("./package");
 server = new Server(config);
 
 server.run();
+
+process.on('SIGINT', function () {
+  server.shutdown();
+});
